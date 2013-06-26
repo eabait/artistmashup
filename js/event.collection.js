b5992d1a3d62404af6211bc8fdfcd0f03fa5fc50
eabait <estebanabait@gmail.com>
2013-06-26 09:44:47 -0300

    foundational code. Communication with Last.FM, ArtistModel and Collection. Basic map with Leaflet

---
+++ js/event.collection.js
@@ -0,0 +1,48 @@
+var EventCollection = (function() {
+
+  return Backbone.Collection.extend({
+
+    // model: ArtistModel,
+
+    parse: function(resp) {
+      var artists = resp.topartists.artist;
+      var models = [];
+
+      // _.each(artists, function(artist) {
+      //   models.push({
+      //     name: artist['name'],
+      //     rank: artist['@attr'].rank,
+      //     listeners: artist['listeners'],
+      //     url: artist['url'],
+      //     images: artist['image']
+      //   });
+      // });
+
+      return models;
+    },
+
+    fetch: function() {
+      var that = this;
+      var def = $.Deferred();
+      LastFmAPI.geo.getTopArtists(
+        {
+          country: 'Argentina',
+          limit: '10'
+        },
+        {
+          success: function(data) {
+            that.parse(data);
+            def.resolve();
+          },
+          error: function(code, message){
+            console.log(code + ' -> ' + message);
+            def.reject();
+          }
+        }
+      );
+      return def;
+    }
+
+  });
+
+})();
