7deb7220ec039d60d80b6bbcc2629814ba121bff
eabait <estebanabait@gmail.com>
2013-06-26 09:55:29 -0300

    set the map according to user geolocation

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
   var artists = new ArtistCollection();
   var promiseOnArtist = artists.fetch();
 
-  var map = L.map('map').fitWorld().zoomIn(1);
+  var map = L.map('map').locate({setView: true, maxZoom: 5});
   L.tileLayer('http://{s}.tile.cloudmade.com/d672d5e3ac6e4841bd45499967b75414/997/256/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
     maxZoom: 18
