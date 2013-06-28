22d73fd3329b8dc839c0288513dddfc415f8d7b0
eabait <estebanabait@gmail.com>
2013-06-28 11:03:31 -0300

    throttled requests

--- js/map/event.collection.js
+++ js/map/event.collection.js
@@ -3,9 +3,12 @@ var EventCollection = (function() {
   return Backbone.Collection.extend({
 
     model: EventModel,
+    count: 0,
 
     onSuccess: function(data) {
       var eventList;
+      this.count += 1;
+      console.log('successful request ' + this.count);
       if (data.events) {
         eventList = data.events.event;
         if (eventList && eventList.length) {
@@ -20,9 +23,10 @@ var EventCollection = (function() {
 
     fetch: function(artists) {
       var that = this;
+      var throttledRequest = _.throttle(LastFmAPI.artist.getEvents, 1);
 
       for (var i = 0, length = artists.length; i < length; ++i) {
-        LastFmAPI.artist.getEvents(
+        throttledRequest(
           {
             artist: artists[i]
           },
